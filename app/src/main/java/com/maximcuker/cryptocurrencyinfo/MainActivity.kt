a1dabfeb49543d3eb88ffe4aebf50bed2741e763
Maximilian6547s <maximilian.s517@gmail.com>
2021-09-09 12:34:04 +0300

    Basic ui components

--- app/src/main/java/com/maximcuker/cryptocurrencyinfo/MainActivity.kt
+++ app/src/main/java/com/maximcuker/cryptocurrencyinfo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.maximcuker.cryptocurrencyinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.maximcuker.cryptocurrencyinfo.ui.theme.CryptocurrencyAppYTTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+
     }
 }
