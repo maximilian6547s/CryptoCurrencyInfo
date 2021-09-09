13a298a00adb445c781feeabf210408a91837c2f
Maximilian6547s <maximilian.s517@gmail.com>
2021-09-09 16:29:17 +0300

    Coin detail screen

--- app/src/main/java/com/maximcuker/cryptocurrencyinfo/presentation/MainActivity.kt
+++ app/src/main/java/com/maximcuker/cryptocurrencyinfo/presentation/MainActivity.kt
@@ -5,15 +5,37 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.maximcuker.cryptocurrencyinfo.presentation.coin_detail.CoinDetailScreen
+import com.maximcuker.cryptocurrencyinfo.presentation.coin_list.CoinListScreen
 import com.maximcuker.cryptocurrencyinfo.presentation.ui.theme.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptocurrencyAppYTTheme {
                 Surface(color = MaterialTheme.colors.background) {
-
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ){
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                           CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
