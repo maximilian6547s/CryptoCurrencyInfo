a109a7edccd12ba397cbea6ae8ee9e804931c543
Maximilian6547s <maximilian.s517@gmail.com>
2021-09-09 14:49:50 +0300

    Setup dependency injection

--- app/src/main/java/com/maximcuker/cryptocurrencyinfo/common/Constants.kt
+++ app/src/main/java/com/maximcuker/cryptocurrencyinfo/common/Constants.kt
@@ -1,4 +1,6 @@
 package com.maximcuker.cryptocurrencyinfo.common
 
 object Constants {
+
+    const val BASE_URL = "https://api.coinpaprika.com/"
 }
---
+++ app/src/main/java/com/maximcuker/cryptocurrencyinfo/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.maximcuker.cryptocurrencyinfo.di
+
+import com.maximcuker.cryptocurrencyinfo.common.Constants.BASE_URL
+import com.maximcuker.cryptocurrencyinfo.data.remote.CoinPaprikaApi
+import com.maximcuker.cryptocurrencyinfo.data.repository.CoinRepositoryImpl
+import com.maximcuker.cryptocurrencyinfo.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
